732884fab35f51f00cce06891d28b25f01776b3b
redaezziani <klausdev2@gmail.com>
2024-08-04 15:42:43 +0100

    chore: Update npm dependencies, add Astro configuration files and dependencies, and update tsconfig and astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+    site:"https://www.xn--tlcharger1xbetlinebet-b5bb.com/",
+  integrations: [tailwind(), react(), sitemap()]
 });
